7dad4e5e7fddf158e9b221f994cf75e9c27613bd
pchernaev <petarchernaev30@gmail.com>
2022-04-22 15:59:28 +0300

    added validation

--- main.js
+++ main.js
@@ -14,20 +14,50 @@ button.addEventListener("click", function () {
     entryDataValue = entryData.value,
     exitDataValue = exitData.value;
 
-  let newCar = {
-    owner: ownerValue,
-    carModel: carModelValue,
-    registration: registrationValue,
-    entryData: entryDataValue,
-    exitData: exitDataValue,
-  };
+  let registrationRegex = /^[A-Z]{1,2}[0-9]{4}[A-Z]{2}$/,
+    entryDataObj = new Date(entryData.value),
+    exitDataObj = new Date(exitData.value),
+    dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
 
-  localData.push(newCar);
-  localStorage.setItem("CLIENTSDATA", JSON.stringify(localData));
+  let err = false,
+    smallReg = document.querySelector("#small-reg"),
+    smallExit = document.querySelector("#small-exit"),
+    smallEntry = document.querySelector("#small-entry");
+  if (!registrationValue.match(registrationRegex)) {
+    smallReg.innerHTML = "Invalid Regestration Number !";
+    err = true;
+  } else {
+    smallReg.innerHTML = "";
+  }
+  if (entryDataObj > exitDataObj) {
+    smallExit.innerHTML = "Invalid Date !";
+    err = true;
+  } else if (
+    !entryDataValue.match(dateRegex) ||
+    !exitDataValue.match(dateRegex)
+  ) {
+    smallExit.innerHTML = "Invalid Date Format !";
+    err = true;
+  } else {
+    smallExit.innerHTML = "";
+  }
 
-  owner.value = "";
-  carModel.value = "";
-  registration.value = "";
-  entryData.value = "";
-  exitData.value = "";
+  if (!err) {
+    let newCar = {
+      owner: ownerValue,
+      carModel: carModelValue,
+      registration: registrationValue,
+      entryData: entryDataValue,
+      exitData: exitDataValue,
+    };
+
+    localData.push(newCar);
+    localStorage.setItem("CLIENTSDATA", JSON.stringify(localData));
+
+    owner.value = "";
+    carModel.value = "";
+    registration.value = "";
+    entryData.value = "";
+    exitData.value = "";
+  }
 });
